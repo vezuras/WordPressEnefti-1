3e7ed67c5463f674320f3913d3c9b078181a84e7
vezuras <74520628+vezuras@users.noreply.github.com>
2024-10-30 15:17:49 -0400

    Update functions.php

--- themes/enefti-child/functions.php
+++ themes/enefti-child/functions.php
@@ -1,4 +1,7 @@
 <?php
+// Commentaire de test pour vérifier l'activation de functions.php dans le thème enfant
+echo "<!-- Commentaire de Test : functions.php est actif dans le thème enfant -->";
+
 function enefti_child_scripts() {
     wp_enqueue_style( 'enefti-parent-style', get_template_directory_uri(). '/style.css' );
 }
